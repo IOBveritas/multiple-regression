ed2ca6d41a7867e8d2977c9303dbfb239e2bf525
Ignat <iob.veritas@gmail.com>
2015-10-09 02:01:37 +0300

    Create oneDimRegression.m

---
+++ oneDimRegression.m
@@ -0,0 +1,49 @@
+function output = oneDimRegression( X, Y, r )
+%ONEDIMREGRESSION - function performs the one-dimentional polynomil
+%regression
+%   X, Y - arrays of coordinates
+%   r - the power of the evaluative polynomial
+%   coefs - the coefficients of the polynomial
+%   Y(x) = A0 + A1 * x + A2 * x^2 + ... + Ar * x ^ r - the form of the polynomial
+
+%   first initialization
+n = numel(X);
+
+%   input validation
+if n ~= numel(Y) || r < 0 || n == 0
+    disp('incorrect input data');
+    return;
+end
+
+%   first elements
+Q = 1 / sqrt(n);
+W = sum(Y) * Q;
+
+if r > 0
+    mean = sum(X) / n;
+    Q(2,1:2) = [-mean / sqrt(sum((X - mean).^2)), 1 / sqrt(sum((X - mean).^2))];
+    W(2) = sum( Y.*polyval(Q(2,end:-1:1),X) );
+    
+    %   building matrix Q, vector w
+    for j = 3:(r+1)
+        %   alpha
+        alpha = sum( X.*(polyval(Q(j-1,end:-1:1),X).^2) );
+        
+        %   beta
+        beta = sum( X.*(polyval(Q(j-1,end:-1:1),X).*polyval(Q(j-2,end:-1:1),X)) );
+
+        %   lambda
+        lambda = sum( ((X - alpha).*polyval(Q(j-1,end:-1:1),X) - beta * polyval(Q(j-2,end:-1:1),X)).^2 );
+        lambda = sqrt(lambda);
+
+        %   next coefficient of Q
+        Q(j,1:j) = ([0 Q(j-1,:)] - alpha * [Q(j-1,:) 0] - beta * [Q(j-2,:) 0]) / lambda;
+        
+        %   calculation Wj
+        W(j) = sum( Y.*polyval(Q(j,end:-1:1),X) );
+    end
+end
+
+output = W*Q;
+
+end
