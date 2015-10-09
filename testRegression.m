b4b564ad6525718d289af8e4ddfaf1e02fbbbde9
unknown <sinyukov@NB122.it.ua>
2015-10-09 22:28:50 +0300

    testRegression corrected

--- testRegression.m
+++ testRegression.m
@@ -1,17 +1,13 @@
 function result = testRegression( polynomial )
 %testRegression calculates the deviation of a polynomial regression
 %   polynomial - the form of regression
+%   result - the standard deviation of one-dimentional regression
 
+sigma = 1;
 r = numel(polynomial) - 1;
-normalDistribution = makedist('Normal','mu',0,'sigma',1);
 X = linspace(-1000,1000,2001);
 n = numel(X);
-Y = zeros(1,n);
-
-for i = 1:n
-    error = normalDistribution.random();
-    Y(i) = polyval(polynomial(end:-1:1),X(i)) + error;
-end
+Y = polyval(polynomial(end:-1:1),X) + sigma*randn(1,n);
 
 evalPolynomial = oneDimRegression(X, Y, r);
 result = standardDeviation(X, Y, evalPolynomial);
