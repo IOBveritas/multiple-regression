0e7a18c6a8a845fd3c8f5064ab5f580285d4a2c8
unknown <sinyukov@NB122.it.ua>
2015-10-11 17:02:42 +0300

    random function change

--- testRegression.m
+++ testRegression.m
@@ -1,13 +1,12 @@
-function result = testRegression( polynomial )
+function result = testRegression( polynomial, sigma )
 %testRegression calculates the deviation of a polynomial regression
 %   polynomial - the form of regression
 %   result - the standard deviation of one-dimentional regression
 
-sigma = 1;
 r = numel(polynomial) - 1;
 X = linspace(-1000,1000,2001);
 n = numel(X);
-Y = polyval(polynomial(end:-1:1),X) + sigma*randn(1,n);
+Y = polyval(polynomial(end:-1:1),X) + normrnd(0, sigma, 1, n);
 
 evalPolynomial = oneDimRegression(X, Y, r);
 result = standardDeviation(X, Y, evalPolynomial);
